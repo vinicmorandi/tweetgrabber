6d42c88629a17b77b27edf082162c27b3acef35e
Vinícius Morandi <viniciuscmorandi@gmail.com>
2022-09-29 10:22:28 -0300

    ajusta metatag

--- components/meta.tsx
+++ components/meta.tsx
@@ -11,7 +11,7 @@ const Meta = () => {
             <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
             <meta
                 name="description"
-                content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+                content={`website para baixar vídeos do twitter gratuitamente - feito por @vinicmorandi`}
             />
         </Head>
     )
